0ecde35105a0b1fb3e705f8dd241436acd2047d8
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-21 04:22:51 +0800

    Addind another hidden layer

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
 bias = [2.0, 3.0, 0.5]
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+bias2 = [-1, 2, -0.5]
 
-layer_outputs = np.dot(inputs, np.array(weights).T) + bias
+layer1_outputs = np.dot(inputs, np.array(weights).T) + bias
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + bias2
 
-print(layer_outputs)
+print(layer2_outputs)
