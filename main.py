774c23f835857e0765856a1846ab7dd185c21f97
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-21 04:51:45 +0800

    Adding layers to create a network of neurons

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-inputs = [[1.0, 2.0, 3.0, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-bias = [2.0, 3.0, 0.5]
-weights2 = [[0.1, -0.14, 0.5],
-            [-0.5, 0.12, -0.33],
-            [-0.44, 0.73, -0.13]]
-bias2 = [-1, 2, -0.5]
+nnfs.init()
 
-layer1_outputs = np.dot(inputs, np.array(weights).T) + bias
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + bias2
+# Dense layer
+class Layer_Dense:
+    # Layer initialization
+    def __init__(self, n_inputs, n_neurons):
+        # Initialize weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-print(layer2_outputs)
+        # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs, weights and biases
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+# Create dataset
+x, y = spiral_data(samples=100, classes=3)
+
+# Create dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense(2, 3)
+
+# Perform a forward pass of our training data through this layer
+dense1.forward(x)
+
+# First few samples
+print(dense1.output[:5])
