b7fa00a1e65d0a87ff33afc859dc783ddc955551
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-21 08:13:46 +0800

    Completed the activation functions

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ nnfs.init()
 
 # Dense layer
 class Layer_Dense:
+
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases
@@ -19,10 +20,25 @@ class Layer_Dense:
 
 # ReLU activation
 class Activation_ReLU:
+    
     # Forward pass
     def forward(self, inputs):
         # Calculate output values from input
         self.output = np.maximum(0, inputs)
+
+# Softmax Activation
+class Activation_Softmax:
+    
+    # Forward pass
+    def forward(self, inputs):
+        
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
 
 # Create dataset
 x, y = spiral_data(samples=100, classes=3)
@@ -33,10 +49,27 @@ dense1 = Layer_Dense(2, 3)
 # Create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
 
-# Perform a forward pass of our training data through this layer
+# Create second Dense layer with 3 input features (as we take output
+# of previous layer here) and 3 output values
+dense2 = Layer_Dense(3, 3)
+
+# Create Softmax activation (to be used with Dense layer):
+activation2 = Activation_Softmax()
+
+# Make a forward pass of our training data through this layer
 dense1.forward(x)
 
+# Make a forward pass through activation function
+# it takes the output of first dense layer here
 activation1.forward(dense1.output)
 
+# Make a forward pass through second Dense layer
+# it takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+
+# Make a forward pass through activation function
+# it takes the output of second dense layer here
+activation2.forward(dense2.output)
+
 # First few samples
-print(activation1.output[:5])
+print(activation2.output[:5])
