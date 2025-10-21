da0a4d5797ec3346ab26028c6b482651819180d4
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-22 04:51:46 +0800

    Created class cross entropy loss

--- nnfs.py
+++ nnfs.py
@@ -49,6 +49,34 @@ class Loss:
         # Return loss
         return data_loss
 
+# Cross-entropy loss
+class Loss_CategoricalCrossentropy(Loss):
+
+    # Forward pass
+    def forward(self, y_pred, y_true):
+
+        # Number of samples in a batch
+        samples = len(y_pred)
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Probabilities for target values -
+        # only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                    range(samples), y_true
+                    ]
+
+        # Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                    y_pred_clipped * y_true,
+                    axis=1
+                    )
+        
+
         # Get unnormalized probabilities
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         
