4533c970f2e0f17928fba7550fce27ac85f1403a
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-24 10:28:55 +0800

    Created some variables

--- nnfs/optimization.py
+++ nnfs/optimization.py
@@ -4,7 +4,20 @@ from nnfs.datasets import vertical_data
 
 nnfs.init()
 
+# Create dataset
 X, y = vertical_data(samples=100, classes=3)
 
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
-plt.show()
+# Create model
+dense1 = Layer_Dense(2, 3) # first dense layer, 2 inputs
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3) # second dense layer, 3 inputs, 3 outputs
+
+# Create loss function
+loss_function = Loss_CategoricalCrossentropy()
+
+# Helper variables
+lowest_loss = 9999999 # some initial value
+best_dense1_weights = 0.05 * np.random.randn(2, 3)
+best_dense1_biases = 0.05 * np.random.randn(1, 3)
+best_dense2_weights = 0.05 * np.random.randn(3, 3)
+best_dense2_biases = 0.05 * np.random.randn(1, 3)
