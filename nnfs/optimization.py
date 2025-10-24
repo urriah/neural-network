6a72d7032d94319e3955233b539daedb33933b72
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-24 10:56:54 +0800

    Added the functions that were missing

--- nnfs/optimization.py
+++ nnfs/optimization.py
@@ -1,8 +1,60 @@
 import matplotlib.pyplot as plt
 import nnfs
 from nnfs.datasets import vertical_data
+import numpy as np
 
 nnfs.init()
+
+# Dense layer
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+# ReLU activation
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+# Softmax activation
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
+
+# Common loss class
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+
+        return data_loss
+
+# Cross-entropy loss
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                    range(samples),
+                    y_true
+                    ]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                    y_pred_clipped * y_true,
+                    axis=1
+                    )
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
 # Create dataset
 X, y = vertical_data(samples=100, classes=3)
@@ -11,6 +63,7 @@ X, y = vertical_data(samples=100, classes=3)
 dense1 = Layer_Dense(2, 3) # first dense layer, 2 inputs
 activation1 = Activation_ReLU()
 dense2 = Layer_Dense(3, 3) # second dense layer, 3 inputs, 3 outputs
+activation2 = Activation_Softmax()
 
 # Create loss function
 loss_function = Loss_CategoricalCrossentropy()
