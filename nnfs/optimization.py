e3095f7b310486977ff7e273e2bef415d89f8c6a
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-24 10:39:58 +0800

    Pick random values for weights and biases

--- nnfs/optimization.py
+++ nnfs/optimization.py
@@ -17,7 +17,39 @@ loss_function = Loss_CategoricalCrossentropy()
 
 # Helper variables
 lowest_loss = 9999999 # some initial value
-best_dense1_weights = 0.05 * np.random.randn(2, 3)
-best_dense1_biases = 0.05 * np.random.randn(1, 3)
-best_dense2_weights = 0.05 * np.random.randn(3, 3)
-best_dense2_biases = 0.05 * np.random.randn(1, 3)
+best_dense1_weights = dense1.weights.copy() 
+best_dense1_biases = dense1.biases.copy() 
+best_dense2_weights = dense2.weights.copy() 
+best_dense2_biases = dense2.biases.copy() 
+
+for iteration in range(10000):
+
+    # Generate a new set of weights for iteration
+    dense1.weights = 0.05 * np.random.randn(2, 3)
+    dense1.biases = 0.05 * np.random.randn(1, 3)
+    dense2.weights = 0.05 * np.random.randn(3, 3)
+    dense2.biases = 0.05 * np.random.randn(1, 3)
+
+    # Perform a forward paass of the training data through this layer
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    # Perform a forward pass through activation function
+    # it takes the output of second dense layer here and returns loss
+    loss = loss_function.calculate(activation2.output, y)
+
+    # Calculate accuracy from output of activation2 and targets
+    # calculate values along first axis
+    predictions = np.argmax(activation2.output, axis=1)
+    accuracy = np.mean(predictions==y)
+
+    # If loss is smaller - print and save weights and biases aside
+    if loss < lowest_loss:
+        print('New set of weights found, iteration:', iteration, 'loss:', loss, 'acc:', accuracy)
+        best_dense1_weights = dense1.weights.copy() 
+        best_dense1_biases = dense1.biases.copy() 
+        best_dense2_weights = dense2.weights.copy() 
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
