c315834cf9e01bf005067be2e0eabd1603f8d3f1
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-26 16:48:52 +0800

    Introducing slope

--- nnfs/derivatives.py
+++ nnfs/derivatives.py
@@ -2,13 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x):
-    return 2*x
+    return 2*x**2
 
 x = np.array(range(5))
 y = f(x)
 
-print(x)
-print(y)
-
-plt.plot(x, y)
-plt.show()
+print((y[1]-y[0]) / (x[1]-x[0]))
+print((y[3]-y[2]) / (x[3]-x[2]))
