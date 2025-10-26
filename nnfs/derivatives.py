f3727ec6231e0948ed20de3dba823d8f09cd42f0
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-26 17:01:53 +0800

    Uncomplete approximate derivative

--- nnfs/derivatives.py
+++ nnfs/derivatives.py
@@ -8,14 +8,16 @@ x = np.arange(0, 5, 0.001)
 y = f(x)
 
 plt.plot(x, y)
-plt.show()
 
 p2_delta = 0.0001
-
-x1 = 1
+x1 = 2
 x2 = x1 + p2_delta # add delta
 
 y1 = f(x1) # result at the derivation point
 y2 = f(x2) # result at the derivation point
 
+print((x1, y1), (x2, y2))
+
+# Derivative approximation and y-intercept for the tangent line
 approximate_derivative = (y2-y1)/(x2-x1)
+b = y2 - approximate_derivative*x2
