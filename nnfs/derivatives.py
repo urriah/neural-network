c26a81defc71043f4fd0f64a19b78fcc65bc7692
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-27 04:08:02 +0800

    Putting the code altogether

--- nnfs/derivatives.py
+++ nnfs/derivatives.py
@@ -21,3 +21,22 @@ print((x1, y1), (x2, y2))
 # Derivative approximation and y-intercept for the tangent line
 approximate_derivative = (y2-y1)/(x2-x1)
 b = y2 - approximate_derivative*x2
+
+# We put the tangent line calculation into a function so we can call
+# it multiple times for different values of x
+# approximate_derivative and b are constant for given function
+# thus calculated once above this function
+def tangent_line(x):
+    return approximate_derivative*x + b
+
+# plotting the tangent line
+# +/- 0.9 to draw the tangent line on our graph
+# then we calculate the y for given x using the tangent line function
+# Matplolib will draw a line for us through these points
+to_plot = [x1-0.9, x1, x1+0.9]
+plt.plot(to_plot, [tangent_line(i) for i in to_plot])
+
+print('Approximate derivative for f(x)',
+      f'where x = {x1} is {approximate_derivative}')
+
+plt.show()
