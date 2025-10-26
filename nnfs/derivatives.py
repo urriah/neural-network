6c160395fa5774aa92e577f269f9b4c4a154afd1
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-26 16:52:07 +0800

    Aprroximate derivative

--- nnfs/derivatives.py
+++ nnfs/derivatives.py
@@ -7,5 +7,13 @@ def f(x):
 x = np.array(range(5))
 y = f(x)
 
-print((y[1]-y[0]) / (x[1]-x[0]))
-print((y[3]-y[2]) / (x[3]-x[2]))
+p2_delta = 0.0001
+
+x1 = 1
+x2 = x1 + p2_delta # add delta
+
+y1 = f(x1) # result at the derivation point
+y2 = f(x2) # result at the derivation point
+
+approximate_derivative = (y2-y1)/(x2-x1)
+print(approximate_derivative)
