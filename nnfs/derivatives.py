ca3058ece85da0c428bddc9134d958973fae260e
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-27 04:31:43 +0800

    Derivative calculated at various points

--- nnfs/derivatives.py
+++ nnfs/derivatives.py
@@ -4,39 +4,41 @@ import numpy as np
 def f(x):
     return 2*x**2
 
-x = np.arange(0, 5, 0.001)
+# np.arrange(start, stop, step) to give us a smoother curve
+x = np.array(np.arange(0, 5, 0.001))
 y = f(x)
 
 plt.plot(x, y)
 
-p2_delta = 0.0001
-x1 = 2
-x2 = x1 + p2_delta # add delta
+colors = ['k','g','r','b','c']
 
-y1 = f(x1) # result at the derivation point
-y2 = f(x2) # result at the derivation point
+def approximate_tangent_line(x, approximate_derivative):
+    return (approximate_derivative*x) + b
 
-print((x1, y1), (x2, y2))
-
-# Derivative approximation and y-intercept for the tangent line
-approximate_derivative = (y2-y1)/(x2-x1)
-b = y2 - approximate_derivative*x2
-
-# We put the tangent line calculation into a function so we can call
-# it multiple times for different values of x
-# approximate_derivative and b are constant for given function
-# thus calculated once above this function
 def tangent_line(x):
     return approximate_derivative*x + b
 
-# plotting the tangent line
-# +/- 0.9 to draw the tangent line on our graph
-# then we calculate the y for given x using the tangent line function
-# Matplolib will draw a line for us through these points
-to_plot = [x1-0.9, x1, x1+0.9]
-plt.plot(to_plot, [tangent_line(i) for i in to_plot])
+for i in range(5):
+    p2_delta = 0.0001
+    x1 = i
+    x2 = x1 + p2_delta
 
-print('Approximate derivative for f(x)',
+    y1 = f(x1)
+    y2 = f(x2)
+
+    print((x1, y1), (x2, y2))
+    approximate_derivative = (y2-y1)/(x2-x1)
+    b = y2 - (approximate_derivative*x2)
+
+    to_plot = [x1-0.9, x1, x1+0.9]
+
+    plt.scatter(x1, y1, c=colors[i])
+    plt.plot(to_plot, [tangent_line(i) for i in to_plot],
+             [approximate_tangent_line(point, approximate_derivative) 
+                for point in to_plot],
+             c=colors[i])
+
+    print('Approximate derivative for f(x)',
       f'where x = {x1} is {approximate_derivative}')
 
 plt.show()
