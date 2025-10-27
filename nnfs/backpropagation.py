9829ac9458fc932481d39c75cd442ab6aa69bc25
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-28 04:49:12 +0800

    Partial derivatives the chain rule

---
+++ nnfs/backpropagation.py
@@ -0,0 +1,35 @@
+# Forward pass
+x = [1.0, -2.0, 3.0] # input values
+w = [-3.0, -1.0, 2.0] # weights
+b = 1.0 # bias
+
+# Multiplying inputs by weights
+xw0 = x[0] * w[0]
+xw1 = x[1] * w[1]
+xw2 = x[2] * w[2]
+
+# Adding weighted inputs and a bias
+z = xw0 + xw1 + xw2 + b
+
+# ReLU activation function
+y = max(z, 0)
+
+# Backward pass
+
+# The derivative from the next layer
+dvalue = 1.0
+
+# Derivative of ReLU and the chain rule
+drelu_dz = dvalue * (1. if z > 0 else 0.)
+print(drelu_dz)
+
+# Partial derivatives of the multiplication, the chain rule
+dsum_dxw0 =1
+dsum_dxw1 = 1
+dsum_dxw2 = 1
+dsum_db = 1
+drelu_dxw0 = drelu_dz * dsum_dxw0
+drelu_dxw1 = drelu_dz * dsum_dxw1
+drelu_dxw2 = drelu_dz * dsum_dxw2
+drelu_db = drelu_dz * dsum_db
+print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
