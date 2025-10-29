05914b973048c71f6f599a70cef58de7c3c880c8
Kirk Urriah <espina.kirkurriah@gmail.com>
2025-10-29 15:17:47 +0800

    Add for other weights and inputs

--- nnfs/backpropagation.py
+++ nnfs/backpropagation.py
@@ -36,5 +36,15 @@ print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
 
 # Partial derivatives of the multiplication, the chain rule
 dmul_dx0 = w[0]
+dmul_dx1 = w[1]
+dmul_dx2 = w[2]
+dmul_dw0 = x[0]
+dmul_dw1 = x[1]
+dmul_dw2 = x[2]
 drelu_dx0 = drelu_dxw0 * dmul_dx0
-print(drelu_dx0)
+drelu_dw0 = drelu_dxw0 * dmul_dw0
+drelu_dx1 = drelu_dxw1 * dmul_dx1
+drelu_dw1 = drelu_dxw1 * dmul_dw1
+drelu_dx2 = drelu_dxw2 * dmul_dx2
+drelu_dw2 = drelu_dxw2 * dmul_dw2
+print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
