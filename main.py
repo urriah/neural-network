de933af1c66c327c873b65bac2fa8830eb1b61c4
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-05-12 13:33:45 +0800

    Layer of neurons

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-inputs = [1.0, 2.0, 3.0, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2.0
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
 
-output = (inputs[0]*weights[0] +
-          inputs[1]*weights[1] +
-          inputs[2]*weights[2] +
-          inputs[3]*weights[3] + bias)
+layer_outputs = []
 
-print(output)
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input*weight
+
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
+
