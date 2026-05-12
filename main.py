c8075ce5121a36ec7ffa5be75b4f32afc13e7576
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-05-12 16:06:12 +0800

    Activation functions

--- main.py
+++ main.py
@@ -13,11 +13,38 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
+class Activation_ReLU:
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+
+    def forward(self, inputs):
+
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
 
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+
+activation2 = Activation_Softmax()
+
 dense1.forward(X)
 
-print(dense1.output[:5])
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
 
