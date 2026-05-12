eb1127737435735b640b0be7cdf0d8616f60f9b1
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-05-12 15:25:56 +0800

    Adding layers done

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import numpy as np
+import nnfs
 from nnfs.datasets import spiral_data
-import matplotlib.pyplot as plt
+
+nnfs.init()
+
+class Layer_Dense:
+
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 X, y = spiral_data(samples=100, classes=3)
 
-plt.scatter(X[:,0], X[:,1])
-plt.show()
+dense1 = Layer_Dense(2, 3)
+
+dense1.forward(X)
+
+print(dense1.output[:5])
+
