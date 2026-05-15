51c568d8963c4d362252dfc109fe99a4c116cd2e
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-05-15 15:35:05 +0800

    Accuracy calculation

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import math
+import numpy as np
 
-softmax_output = [0.7, 0.1, 0.2]
+softmax_outputs = np.array([[0.7, 0.2, 0.1],
+                            [0.5, 0.1, 0.4],
+                            [0.02, 0.9, 0.08]])
+class_targets = np.array([0, 1, 1])
 
-target_output = [1, 0, 0]
+predictions = np.argmax(softmax_outputs, axis=1)
+if len(class_targets.shape) == 2:
+    class_targets = np.argmax(class_targets, axis=1)
+accuracy = np.mean(predictions==class_targets)
 
-loss = -(math.log(softmax_output[0])*target_output[0] +
-         math.log(softmax_output[1])*target_output[1] +
-         math.log(softmax_output[2])*target_output[2])
-
-print(loss)
+print('acc:', accuracy)
 
