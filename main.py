92b61ddb3a5f75ecd1120df2fb63ed7f9c0523db
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-05-18 20:58:04 +0800

    Introduction optimization

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import vertical_data
 
-softmax_outputs = np.array([[0.7, 0.2, 0.1],
-                            [0.5, 0.1, 0.4],
-                            [0.02, 0.9, 0.08]])
-class_targets = np.array([0, 1, 1])
+nnfs.init()
 
-predictions = np.argmax(softmax_outputs, axis=1)
-if len(class_targets.shape) == 2:
-    class_targets = np.argmax(class_targets, axis=1)
-accuracy = np.mean(predictions==class_targets)
+X, y = vertical_data(samples=100, classes=3)
 
-print('acc:', accuracy)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+plt.show()
 
