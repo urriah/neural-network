c89bf836534950a3fd88f72e37c0af00f8e9f334
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-06-22 17:14:13 +0800

    How we will be applying learning rate decay

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 starting_learning_rate = 1.
 learning_rate_decay = 0.1
-step = 1
 
-learning_rate = starting_learning_rate * \
-		(1. / (1 + learning_rate_decay * step))
-print(learning_rate)
+for step in range(20):
+	learning_rate = starting_learning_rate * \
+			(1. / (1 + learning_rate_decay * step))
+	print(learning_rate)
