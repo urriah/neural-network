0556553ac2ae916e7c730b97ae32eccccb27538c
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-06-24 17:15:23 +0800

    lambda regularizer strength hyperparameters

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from nnfs.datasets import spiral_data
 nnfs.init()
 
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        # Initialize weights and biases
+    def __init__(self, n_inputs, n_neurons,
+	weight_regularizer_L1=0, weight_regularizer_L2=0,
+	bias_regularizer_L1=0, bias_regularizer_L2=0):
+
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+
+	self.weight_regularizer_l1 = weight_regularizer_l1
+	self.weight_regularizer_l2 = weight_regularizer_l2
+	self.bias_regularizer_l1 = bias_regularizer_l1
+	self.bias_regularizer_l2 = bias_regularizer_l2
 
     def forward(self, inputs):
         self.inputs = inputs
