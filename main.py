5b3d07fd0db422a601b6f322689593ffe59bba27
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-06-25 16:42:45 +0800

    Created mean squeared error class

--- main.py
+++ main.py
@@ -367,6 +367,19 @@ class Loss_BinaryCrossentropy(Loss):
                          (1 - y_true) / (1 - clipped_dvalues)) / outputs
         self.dinputs = self.dinputs / samples
 
+class Loss_MeanSquaredError(Loss):
+    def forward(self, y_pred, y_true):
+        sample_losses =  np.mean((y_true - y_pred)**2, axis=-1)
+
+    return sample_losses
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        outputs = len(dvalues[0])
+
+        self.dinputs = -2 * (y_true - dvalues) / outputs
+        self.dinputs = self.dinputs / samples
+
 X, y = spiral_data(100, 2)
 
 y = y.reshape(-1, 1)
