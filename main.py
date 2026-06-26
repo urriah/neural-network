01c9dbd06f4d7a39a0d313eee5547b43bedc9595
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-06-26 22:19:43 +0800

    Created the model class and other classes

--- main.py
+++ main.py
@@ -67,6 +67,9 @@ class Activation_ReLU:
         self.dinputs = dvalues.copy()
         self.dinputs[self.inputs <= 0] = 0
 
+    def predictions(self, outputs):
+        return outputs
+
 class Activation_Softmax:
     def forward(self, inputs):
         self.inputs = inputs
@@ -89,6 +92,8 @@ class Activation_Softmax:
                               np.dot(single_output, single_output.T)
             self.dinputs[index] = np.dot(jacobian_matrix,
                                          single_dvalues)
+    def predictions(self, outputs):
+        return np.argmax(outputs, axis=1)
 
 class Activation_Sigmoid:
     def forward(self, inputs):
@@ -97,6 +102,9 @@ class Activation_Sigmoid:
 
     def backward(self, dvalues):
         self.dinputs = dvalues * (1 - self.output) * self.output
+
+    def predictions(self, outputs):
+        return (outputs > 0.5) * 1
 
 class Optimizer_SGD:
     def __init__(self, learning_rate=1., decay=0., momentum=0.):
@@ -267,28 +275,38 @@ class Loss:
 
         return data_loss
 
-    def regularization_loss(self, layer):
+    def regularization_loss(self):
         regularization_loss = 0
 
-        if layer.weight_regularizer_l1 > 0:
-            regularization_loss += layer.weight_regularizer_l1 * \
-	    			   np.sum(np.abs(layer.weights))
+        for layer in self.trainable_layers:
+            if layer.weight_regularizer_l1 > 0:
+                regularization_loss += layer.weight_regularizer_l1 * \
+                                       np.sum(np.abs(layer.weights))
 
-        if layer.weight_regularizer_l2 > 0:
-            regularization_loss += layer.weight_regularizer_l2 * \
-				   np.sum(layer.weights * \
-					  layer.weights)
+            if layer.weight_regularizer_l2 > 0:
+                regularization_loss += layer.weight_regularizer_l2 * \
+                                       np.sum(layer.weights * \
+                                              layer.weights)
 
-        if layer.bias_regularizer_l1 > 0:
-            regularization_loss += layer.bias_regularizer_l1 * \
-				  np.sum(np.abs(layer.biases))
+            if layer.bias_regularizer_l1 > 0:
+                regularization_loss += layer.bias_regularizer_l1 * \
+                                       np.sum(np.abs(layer.biases))
 
-        if layer.bias_regularizer_l2 > 0:
-            regularization_loss += layer.bias_regularizer_l2 * \
-				  np.sum(layer.biases * \
-					 layer.biases)
+            if layer.bias_regularizer_l2 > 0:
+                regularization_loss += layer.bias_regularizer_l2 * \
+                                       np.sum(layer.biases * \
+                                              layer.biases)
 
         return regularization_loss
+
+    def remember_trainable_layers(self, trainable_Layers):
+        self.trainable_layers = trainable_layers
+
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+
+        return data_loss, self.regularization_loss()
 
 class Loss_CategoricalCrossentropy(Loss):
     def forward(self, y_pred, y_true):
@@ -348,6 +366,9 @@ class Activation_Linear:
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
 
+    def predictions(self, outputs):
+        return outputs
+
 class Loss_BinaryCrossentropy(Loss):
     def forward(self, y_pred, y_true):
         y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
@@ -394,83 +415,77 @@ class Loss_MeanAbsoluteError(Loss):
         self.dinputs = np.sign(y_true - dvalues) / outputs
         self.dinputs = self.dinputs / samples
 
+class Layer_Input():
+    def forward(self, inputs):
+        self.output = inputs
+
+class Model():
+    def __init__(self):
+        self.layers = []
+
+    def add(self, layer):
+        self.layers.append(layer)
+
+    def set(self, *, Loss, optimizer):
+        self.loss = Loss
+        self.optimizer = optimizer
+
+    def finalize(self):
+        self.input_layer = Layer_Input()
+        layer_count = len(self.layers)
+        self.trainable_layers = []
+
+        for i in range(layer_count):
+            if i == 0:
+                self.layers[i].prev = self.input_layer
+                self.layers[i].next = self.layers[i+1]
+            elif i < layer_count - 1:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.layers[i+1]
+            else:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.loss
+                
+        if hasattr(self.layers[i], 'weights'):
+            self.trainable_layers.append(self.layers[i])
+        else:
+            self.layers[i].prev = self.layers[i-1]
+            self.layers[i].next = self.loss
+            self.output_layer_activation = self.layers[i]
+
+    def train(self, X, y, *, epochs=1, print_every=1):
+        for epoch in range(1, epochs+1):
+            output = self.forward(X)
+
+            print(output)
+            exit()
+
+    def forward(self, X):
+        self.input_layer.forward(X)
+
+        for layer in self.layers:
+            layer.forward(layer.prev.output)
+
+        return layer.output
+
 X, y = sine_data()
 
-dense1 = Layer_Dense(1, 64)
+model = Model()
 
-activation1 = Activation_ReLU()
+model.add(Layer_Dense(1, 64))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(64, 64))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(64, 1))
+model.add(Activation_Linear())
 
-dense2 = Layer_Dense(64, 64)
+model.set(
+    Loss=Loss_MeanSquaredError(),
+    optimizer=Optimizer_Adam(learning_rate=0.005, decay=1e-3),
+)
 
-activation2 = Activation_ReLU()
+model.finalize()
 
-dense3 = Layer_Dense(64, 1)
+model.train(X, y, epochs = 10000, print_every=100)
 
-activation3 = Activation_Linear()
-
-loss_function = Loss_MeanSquaredError()
-
-optimizer = Optimizer_Adam(learning_rate=0.005, decay=1e-3)
-
-accuracy_position = np.std(y) / 250
-
-for epoch in range(10001):
-    dense1.forward(X)
-
-    activation1.forward(dense1.output)
-    
-    dense2.forward(activation1.output)
-
-    activation2.forward(dense2.output)
-
-    dense3.forward(activation2.output)
-
-    activation3.forward(dense3.output)
-
-    data_loss = loss_function.calculate(activation3.output, y)
-
-    regularization_loss = \
-        loss_function.regularization_loss(dense1) + \
-        loss_function.regularization_loss(dense2) + \
-        loss_function.regularization_loss(dense3)
-
-    loss = data_loss + regularization_loss
-
-    predictions = activation3.output
-    accuracy = np.mean(np.absolute(predictions - y) <
-                       accuracy_position)
-
-    if not epoch % 100:
-        print(f'epoch: {epoch}, ' +
-              f'acc: {accuracy:.3f}, ' +
-              f'loss: {loss:.3f}, (' +
-	      f'data_loss: {data_loss:.3f}, ' +
-	      f'reg_loss: {regularization_loss:.3f}), ' +
-              f'lr: {optimizer.current_learning_rate}')
-
-    loss_function.backward(activation3.output, y)
-    activation3.backward(loss_function.dinputs)
-    dense3.backward(activation3.dinputs)
-    activation2.backward(dense3.dinputs)
-    dense2.backward(activation2.dinputs)
-    activation1.backward(dense2.dinputs)
-    dense1.backward(activation1.dinputs)
-
-    optimizer.pre_update_params()
-    optimizer.update_params(dense1)
-    optimizer.update_params(dense2)
-    optimizer.update_params(dense3)
-    optimizer.post_update_params()
-
-X_test, y_test = sine_data()
-
-dense1.forward(X_test)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-dense3.forward(activation2.output)
-activation3.forward(dense3.output)
-
-plt.plot(X_test, y_test)
-plt.plot(X_test, activation3.output)
-plt.show()
+print(model.layers)
